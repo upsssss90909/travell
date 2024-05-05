7879f8787a274bad792272931049165990690a8c
uday <89901661+udayPS14@users.noreply.github.com>
2024-05-05 07:00:31 +0530

    changed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -61,13 +61,13 @@ const Hero = () => {
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Himachal Pradesh</p>
           </div>
 
           <div className="flexBetween">
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Distance</p>
-              <p className="bold-20 text-white">173.28 mi</p>
+              <p className="bold-20 text-white">250Km</p>
             </div>
             <div className="flex flex-col">
               <p className="regular-16 block text-gray-20">Elevation</p>
